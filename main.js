b8ea38d98197034907124887a8dfbc67559b49a5
Eloquent0 <sakatats@vcu.edu>
2026-02-21 17:48:54 -0500

    Ver 0.1.3

--- main.js
+++ main.js
@@ -714,3 +714,13 @@ document.addEventListener("DOMContentLoaded", () => {
   ],
   100
 );
+(function animateFavicon(frames, interval) {
+  let i = 0;
+  const link = document.querySelector("link[rel~='icon']") || document.createElement('link');
+  link.rel = 'icon';
+  document.head.appendChild(link);
+  setInterval(() => {
+    link.href = "data:image/gif;base64," + frames[i % frames.length];
+    i++;
+  }, interval);
+})(GIF_DATA, 100);
